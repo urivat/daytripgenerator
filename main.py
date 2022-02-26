d11deb9c6f498abea7668314df3d8559865f209c
rashad porter <rashad.porter18@gmail.com>
2022-02-26 10:03:51 -0500

    is this working

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # (5 points): As a developer, I want to make at least three commits with descriptive messages.  
+# def pick_an_answer():
+#     user_input = input('y/n')
+#     while user_input != 'y':
+#         print(user_input)
+# answer = pick_an_answer()
+# print(answer)
+
+
 
 # (5 points): As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainments in their own separate lists.  
 destinations = ['Atlanta', 'Nashville', 'Tokyo', 'Sydney', 'The Moon' , 'Hawaii', 'New York', 'Miami' ]
@@ -6,69 +14,73 @@ restaurants = ['lumber jacks', "Lily's BBQ", 'Pho Cafe', 'four seasons', "hell's
 Transportation = ['car', 'bullet train', 'slow train', 'horse and carriage', 'boat', 'spaceship', 'hotwheel']
 entertainment = ['State park', 'izakaya', 'six flags', 'basketball game', 'explore the city', 'A japanese shrine','beachday','drive into a crater']
 
-
-# (5 points): As a user, I want a destination to be randomly selected for my day trip.  
-# from math import pi
 import random
 def pick_a_destination(list_of_destinations):
-    random_destination = random.choice(spots)
+    random_destination = random.choice(list_of_destinations)
     return random_destination
-spots = destinations
-results = pick_a_destination(spots)
-print(f'Congratulations we have randomly selected your trip for today going to {results} is this ok? ')
-
-# (5 points): As a user, I want a restaurant to be randomly selected for my day trip.  
+result_des = pick_a_destination(destinations)
+print(f'Congratulations we have randomly selected your trip for today going to {result_des} is this ok? ')
+selection = input('y/n ')
+if selection == 'n':
+    result_des = pick_a_destination(destinations)
+    print(f'Congratulations we have randomly selected your trip for today going to {result_des} is this ok? ')
+    input('y/n ')
+# user will respond with yes or no
+# if statement to create two paths
+# no? recall function
 def pick_a_restaurant(list_of_restaurants):
-    random_restaurant = random.choice(eats)
+    random_restaurant = random.choice(list_of_restaurants)
     return random_restaurant
-eats = restaurants
-results_rest = pick_a_restaurant(eats)
+results_rest = pick_a_restaurant(restaurants)
 print(f'The restaurant choice is {results_rest} is this acceptable? ')
+selection = input('y/n ')
+if selection == 'n':
+    results_rest= pick_a_restaurant(restaurants)
+    print(f'The restaurant choice is {results_rest} is this acceptable? ')
+    input('y/n ')
 
-# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.  
+
 def pick_a_mode_of_transport(list_of_transportation_options):
-    random_transportation = random.choice(vehicles)
+    random_transportation = random.choice(list_of_transportation_options)
     return random_transportation
-vehicles= Transportation
-type_of_vic = pick_a_mode_of_transport(vehicles)
+type_of_vic = pick_a_mode_of_transport(Transportation)
 print(f'Your mode of travel will be by {type_of_vic}')
-
-# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.  
+selection = input('y/n ')
+if selection == 'n':
+   print(f'Your mode of travel will be by {type_of_vic}')  
+   input('y/n ')   
 def pick_some_entertainment(list_of_fun):
-    random_fun = random.choice(fun)
+    random_fun = random.choice(list_of_fun)
     return random_fun
-fun = entertainment
-types_of_fun = pick_some_entertainment(fun)
-print(f'You will experience a day of fun at {types_of_fun} is this accptable')
-
+types_of_fun = pick_some_entertainment(entertainment)
+print(f'You will experience a day of fun at {types_of_fun} is this accptable? ')
+selection = input('y/n ')
+if selection == 'n':
+    print(f'You will experience a day of fun at {types_of_fun} is this accptable? ')    
+    input('y/n ')
+print('Congrats, your trip has been completely generated. Just to confirm this is your trip below')
+def Confirmation_list():
+    print(f'destination: {result_des}')
+    print(f'restaurant: {results_rest}')
+    print(f'travel type: {type_of_vic}')
+    print(f'entertainment: {types_of_fun}')
+Confirmation_list()
+selection = input("everything look good for you? enter: y/n ")
+if selection == 'n':
+    print("sorry we couldnt help you're welcome to try again")
+else:
+    print('Perfect, glad we could help you with your decision! ')
+print(f'Hope you are prepared for a wonderful trip. we have you arriving at {result_des} then having a nice lunch at {results_rest}. For the fun of the day you will travel by {type_of_vic} going around the area with a {types_of_fun}')
+# (5 points): As a user, I want a destination to be randomly selected for my day trip.  
+# from math import pi
+# (5 points): As a user, I want a restaurant to be randomly selected for my day trip.  
+# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.  
+# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.  
 # (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.  
-# this below function is responsible for reselecting list results
-def random_reselect():
-    user_pick = input('is this acceptable? enter: y/n ')
-    while user_pick != 'y':
-        print(f'sorry, you dont like that option what about   enter y/n')
-
-      
-user_select= 'answer'    
-focus = random_reselect()
-print(focus)
-
-
-
-# question = input()    
-# the_result = yey_or_nay
-# print(the_result)
-    
-
-# print('perfect, lets move on')
-    
-
-
-
-
-    
-
 # (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.  
+
+
+
 
 # (10 points): As a user, I want to display my completed trip in the console.  
 
